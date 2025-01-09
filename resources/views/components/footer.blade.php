aadf4037e4d27bdd51d29a42229ddafba30e1895
JudgementKnight <rangdajiednongbet@gmail.com>
2025-01-09 10:37:31 +0530

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -8,22 +8,7 @@
                     </a>
                 </p>
             </div>
-            <div class="col-6 text-end">
-                <ul class="list-inline">
-                    <li class="list-inline-item">
-                        <a href="#" class="text-muted">Contact</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#" class="text-muted">About Us</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#" class="text-muted">Terms</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#" class="text-muted">Booking</a>
-                    </li>
-                </ul>
-            </div>
+     
         </div>
     </div>
 </footer>
